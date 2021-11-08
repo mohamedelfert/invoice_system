87666a785fa63e5e53c9c1542f737bbebb08d87b
mohamedelfert <me_me4152@yahoo.com>
2021-11-08 18:34:00 +0200

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -182,179 +182,179 @@
     </div>
     <!-- row closed -->
 
-    <!-- row opened -->
-    <div class="row row-sm">
+{{--    <!-- row opened -->--}}
+{{--    <div class="row row-sm">--}}
 
-        <div class="col-md-12 col-lg-4 col-xl-4">
-                <div class="card card-dashboard-eight pb-2">
-                    <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>
-                    <div class="list-group">
-                        <div class="list-group-item border-top-0">
-                            <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-                            <p>United States</p><span>$1,671.10</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-nl flag-icon-squared"></i>
-                            <p>Netherlands</p><span>$1,064.75</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
-                            <p>United Kingdom</p><span>$1,055.98</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
-                            <p>Canada</p><span>$1,045.49</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-in flag-icon-squared"></i>
-                            <p>India</p><span>$1,930.12</span>
-                        </div>
-                        <div class="list-group-item border-bottom-0 mb-0">
-                            <i class="flag-icon flag-icon-au flag-icon-squared"></i>
-                            <p>Australia</p><span>$1,042.00</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--        <div class="col-md-12 col-lg-4 col-xl-4">--}}
+{{--                <div class="card card-dashboard-eight pb-2">--}}
+{{--                    <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>--}}
+{{--                    <div class="list-group">--}}
+{{--                        <div class="list-group-item border-top-0">--}}
+{{--                            <i class="flag-icon flag-icon-us flag-icon-squared"></i>--}}
+{{--                            <p>United States</p><span>$1,671.10</span>--}}
+{{--                        </div>--}}
+{{--                        <div class="list-group-item">--}}
+{{--                            <i class="flag-icon flag-icon-nl flag-icon-squared"></i>--}}
+{{--                            <p>Netherlands</p><span>$1,064.75</span>--}}
+{{--                        </div>--}}
+{{--                        <div class="list-group-item">--}}
+{{--                            <i class="flag-icon flag-icon-gb flag-icon-squared"></i>--}}
+{{--                            <p>United Kingdom</p><span>$1,055.98</span>--}}
+{{--                        </div>--}}
+{{--                        <div class="list-group-item">--}}
+{{--                            <i class="flag-icon flag-icon-ca flag-icon-squared"></i>--}}
+{{--                            <p>Canada</p><span>$1,045.49</span>--}}
+{{--                        </div>--}}
+{{--                        <div class="list-group-item">--}}
+{{--                            <i class="flag-icon flag-icon-in flag-icon-squared"></i>--}}
+{{--                            <p>India</p><span>$1,930.12</span>--}}
+{{--                        </div>--}}
+{{--                        <div class="list-group-item border-bottom-0 mb-0">--}}
+{{--                            <i class="flag-icon flag-icon-au flag-icon-squared"></i>--}}
+{{--                            <p>Australia</p><span>$1,042.00</span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
-        <div class="col-md-12 col-lg-8 col-xl-8">
-            <div class="card card-table-two">
-                <div class="d-flex justify-content-between">
-                    <h4 class="card-title mb-1">Your Most Recent Earnings</h4>
-                    <i class="mdi mdi-dots-horizontal text-gray"></i>
-                </div>
-                <span class="tx-12 tx-muted mb-3 ">This is your most recent earnings for today's date.</span>
-                <div class="table-responsive country-table">
-                    <table class="table table-striped table-bordered mb-0 text-sm-nowrap text-lg-nowrap text-xl-nowrap">
-                        <thead>
-                        <tr>
-                            <th class="wd-lg-25p">Date</th>
-                            <th class="wd-lg-25p tx-right">Sales Count</th>
-                            <th class="wd-lg-25p tx-right">Earnings</th>
-                            <th class="wd-lg-25p tx-right">Tax Witheld</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>05 Dec 2019</td>
-                            <td class="tx-right tx-medium tx-inverse">34</td>
-                            <td class="tx-right tx-medium tx-inverse">$658.20</td>
-                            <td class="tx-right tx-medium tx-danger">-$45.10</td>
-                        </tr>
-                        <tr>
-                            <td>06 Dec 2019</td>
-                            <td class="tx-right tx-medium tx-inverse">26</td>
-                            <td class="tx-right tx-medium tx-inverse">$453.25</td>
-                            <td class="tx-right tx-medium tx-danger">-$15.02</td>
-                        </tr>
-                        <tr>
-                            <td>07 Dec 2019</td>
-                            <td class="tx-right tx-medium tx-inverse">34</td>
-                            <td class="tx-right tx-medium tx-inverse">$653.12</td>
-                            <td class="tx-right tx-medium tx-danger">-$13.45</td>
-                        </tr>
-                        <tr>
-                            <td>08 Dec 2019</td>
-                            <td class="tx-right tx-medium tx-inverse">45</td>
-                            <td class="tx-right tx-medium tx-inverse">$546.47</td>
-                            <td class="tx-right tx-medium tx-danger">-$24.22</td>
-                        </tr>
-                        <tr>
-                            <td>09 Dec 2019</td>
-                            <td class="tx-right tx-medium tx-inverse">31</td>
-                            <td class="tx-right tx-medium tx-inverse">$425.72</td>
-                            <td class="tx-right tx-medium tx-danger">-$25.01</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+{{--        <div class="col-md-12 col-lg-8 col-xl-8">--}}
+{{--            <div class="card card-table-two">--}}
+{{--                <div class="d-flex justify-content-between">--}}
+{{--                    <h4 class="card-title mb-1">Your Most Recent Earnings</h4>--}}
+{{--                    <i class="mdi mdi-dots-horizontal text-gray"></i>--}}
+{{--                </div>--}}
+{{--                <span class="tx-12 tx-muted mb-3 ">This is your most recent earnings for today's date.</span>--}}
+{{--                <div class="table-responsive country-table">--}}
+{{--                    <table class="table table-striped table-bordered mb-0 text-sm-nowrap text-lg-nowrap text-xl-nowrap">--}}
+{{--                        <thead>--}}
+{{--                        <tr>--}}
+{{--                            <th class="wd-lg-25p">Date</th>--}}
+{{--                            <th class="wd-lg-25p tx-right">Sales Count</th>--}}
+{{--                            <th class="wd-lg-25p tx-right">Earnings</th>--}}
+{{--                            <th class="wd-lg-25p tx-right">Tax Witheld</th>--}}
+{{--                        </tr>--}}
+{{--                        </thead>--}}
+{{--                        <tbody>--}}
+{{--                        <tr>--}}
+{{--                            <td>05 Dec 2019</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">34</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">$658.20</td>--}}
+{{--                            <td class="tx-right tx-medium tx-danger">-$45.10</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td>06 Dec 2019</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">26</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">$453.25</td>--}}
+{{--                            <td class="tx-right tx-medium tx-danger">-$15.02</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td>07 Dec 2019</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">34</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">$653.12</td>--}}
+{{--                            <td class="tx-right tx-medium tx-danger">-$13.45</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td>08 Dec 2019</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">45</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">$546.47</td>--}}
+{{--                            <td class="tx-right tx-medium tx-danger">-$24.22</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td>09 Dec 2019</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">31</td>--}}
+{{--                            <td class="tx-right tx-medium tx-inverse">$425.72</td>--}}
+{{--                            <td class="tx-right tx-medium tx-danger">-$25.01</td>--}}
+{{--                        </tr>--}}
+{{--                        </tbody>--}}
+{{--                    </table>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-    </div>
-    <!-- row close -->
+{{--    </div>--}}
+{{--    <!-- row close -->--}}
 
-    <!-- row opened -->
-    <div class="row row-sm row-deck">
+{{--    <!-- row opened -->--}}
+{{--    <div class="row row-sm row-deck">--}}
 
-        <div class="col-md-12 col-lg-4 col-xl-4">
-            <div class="card card-dashboard-eight pb-2">
-                <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>
-                <div class="list-group">
-                    <div class="list-group-item border-top-0">
-                        <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-                        <p>United States</p><span>$1,671.10</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-nl flag-icon-squared"></i>
-                        <p>Netherlands</p><span>$1,064.75</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
-                        <p>United Kingdom</p><span>$1,055.98</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
-                        <p>Canada</p><span>$1,045.49</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-in flag-icon-squared"></i>
-                        <p>India</p><span>$1,930.12</span>
-                    </div>
-                    <div class="list-group-item border-bottom-0 mb-0">
-                        <i class="flag-icon flag-icon-au flag-icon-squared"></i>
-                        <p>Australia</p><span>$1,042.00</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="col-md-12 col-lg-4 col-xl-4">--}}
+{{--            <div class="card card-dashboard-eight pb-2">--}}
+{{--                <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>--}}
+{{--                <div class="list-group">--}}
+{{--                    <div class="list-group-item border-top-0">--}}
+{{--                        <i class="flag-icon flag-icon-us flag-icon-squared"></i>--}}
+{{--                        <p>United States</p><span>$1,671.10</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-nl flag-icon-squared"></i>--}}
+{{--                        <p>Netherlands</p><span>$1,064.75</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-gb flag-icon-squared"></i>--}}
+{{--                        <p>United Kingdom</p><span>$1,055.98</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-ca flag-icon-squared"></i>--}}
+{{--                        <p>Canada</p><span>$1,045.49</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-in flag-icon-squared"></i>--}}
+{{--                        <p>India</p><span>$1,930.12</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item border-bottom-0 mb-0">--}}
+{{--                        <i class="flag-icon flag-icon-au flag-icon-squared"></i>--}}
+{{--                        <p>Australia</p><span>$1,042.00</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <div class="col-xl-4 col-md-12 col-lg-6">
-            <div class="card">
-                <div class="card-header pb-0">
-                    <h3 class="card-title mb-2">Recent Orders</h3>
-                    <p class="tx-12 mb-0 text-muted">An order is an investor's instructions to a broker or brokerage firm to purchase or sell</p>
-                </div>
-                <div class="card-body sales-info ot-0 pt-0 pb-0">
+{{--        <div class="col-xl-4 col-md-12 col-lg-6">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-header pb-0">--}}
+{{--                    <h3 class="card-title mb-2">Recent Orders</h3>--}}
+{{--                    <p class="tx-12 mb-0 text-muted">An order is an investor's instructions to a broker or brokerage firm to purchase or sell</p>--}}
+{{--                </div>--}}
+{{--                <div class="card-body sales-info ot-0 pt-0 pb-0">--}}
 
-                </div>
-            </div>
-        </div>
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <div class="col-md-12 col-lg-4 col-xl-4">
-            <div class="card card-dashboard-eight pb-2">
-                <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>
-                <div class="list-group">
-                    <div class="list-group-item border-top-0">
-                        <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-                        <p>United States</p><span>$1,671.10</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-nl flag-icon-squared"></i>
-                        <p>Netherlands</p><span>$1,064.75</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
-                        <p>United Kingdom</p><span>$1,055.98</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
-                        <p>Canada</p><span>$1,045.49</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-in flag-icon-squared"></i>
-                        <p>India</p><span>$1,930.12</span>
-                    </div>
-                    <div class="list-group-item border-bottom-0 mb-0">
-                        <i class="flag-icon flag-icon-au flag-icon-squared"></i>
-                        <p>Australia</p><span>$1,042.00</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="col-md-12 col-lg-4 col-xl-4">--}}
+{{--            <div class="card card-dashboard-eight pb-2">--}}
+{{--                <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>--}}
+{{--                <div class="list-group">--}}
+{{--                    <div class="list-group-item border-top-0">--}}
+{{--                        <i class="flag-icon flag-icon-us flag-icon-squared"></i>--}}
+{{--                        <p>United States</p><span>$1,671.10</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-nl flag-icon-squared"></i>--}}
+{{--                        <p>Netherlands</p><span>$1,064.75</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-gb flag-icon-squared"></i>--}}
+{{--                        <p>United Kingdom</p><span>$1,055.98</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-ca flag-icon-squared"></i>--}}
+{{--                        <p>Canada</p><span>$1,045.49</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item">--}}
+{{--                        <i class="flag-icon flag-icon-in flag-icon-squared"></i>--}}
+{{--                        <p>India</p><span>$1,930.12</span>--}}
+{{--                    </div>--}}
+{{--                    <div class="list-group-item border-bottom-0 mb-0">--}}
+{{--                        <i class="flag-icon flag-icon-au flag-icon-squared"></i>--}}
+{{--                        <p>Australia</p><span>$1,042.00</span>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-    </div>
-    <!-- /row -->
+{{--    </div>--}}
+{{--    <!-- /row -->--}}
     </div>
     </div>
     <!-- Container closed -->
