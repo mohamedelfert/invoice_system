64e61a32c4fdf65b12cc11e2e97e09a2abbd38b9
mohamedelfert <me_me4152@yahoo.com>
2021-11-13 13:30:47 +0200

    edit logo

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -3,11 +3,11 @@
 				<div class="container-fluid">
 					<div class="main-header-left ">
 						<div class="responsive-logo">
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo.png')}}" class="logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/logo-white.png')}}" class="dark-logo-1" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="logo-2" alt="logo"></a>
-							<a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="dark-logo-2" alt="logo"></a>
-						</div>
+							<a href="{{ url('/' . $page='home') }}"><img src="{{URL::asset('assets/img/brand/invologo.png')}}" class="logo-1" alt="logo"></a>
+							<a href="{{ url('/' . $page='home') }}"><img src="{{URL::asset('assets/img/brand/invologo.png')}}" class="logo-2" alt="logo"></a>
+                            <a href="{{ url('/' . $page='home') }}"><img src="{{URL::asset('assets/img/brand/invologo.png')}}" class="dark-logo-2" alt="logo"></a>
+                            <a href="{{ url('/' . $page='home') }}"><img src="{{URL::asset('assets/img/brand/invologo.png')}}" class="dark-logo-1" alt="logo"></a>
+                        </div>
 						<div class="app-sidebar__toggle" data-toggle="sidebar">
 							<a class="open-toggle" href="#"><i class="header-icon fe fe-align-left" ></i></a>
 							<a class="close-toggle" href="#"><i class="header-icons fe fe-x"></i></a>
@@ -84,11 +84,11 @@
 								<a class="new nav-link full-screen-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg></a>
 							</div>
 							<div class="dropdown main-profile-menu nav nav-item nav-link">
-								<a class="profile-user d-flex" href=""><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}"></a>
+								<a class="profile-user d-flex" href=""><img alt="" src="{{URL::asset('assets/img/faces/avatar.jpg')}}"></a>
 								<div class="dropdown-menu">
 									<div class="main-header-profile bg-primary p-3">
 										<div class="d-flex wd-100p">
-											<div class="main-img-user"><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}" class=""></div>
+											<div class="main-img-user"><img alt="" src="{{URL::asset('assets/img/faces/avatar.jpg')}}" class=""></div>
 											<div class="mr-3 my-auto">
 												<h6>{{auth()->user()->name}}</h6><span>{{auth()->user()->email}}</span>
 											</div>
